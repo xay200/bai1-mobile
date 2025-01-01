cadf5b96745ee7615cb91c388f712014c5670799
Pham Chien <phamchien5870@gmail.com>
2025-01-01 21:18:57 +0700

    hello

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, KeyboardAvoidingView, Keyboard } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.baseText}>Đăng nhập</Text>
+      <Text>Nhập số điện thoại</Text>
+      <Text>Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro</Text>
+      <KeyboardAvoidingView behavior='padding'>
+        
+        <TextInput 
+        style= {styles.input}
+        placeholder="Nhập số điện thoại của bạn"
+        value ={Number}
+        keyboardType='phone-pad'
+      />
+      </KeyboardAvoidingView>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +27,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    top: 20
   },
+  baseText: {
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40
+  }
 });
